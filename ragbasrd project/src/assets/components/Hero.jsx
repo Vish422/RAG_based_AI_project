9544032a984c7750dc2aa49c1685bca13cf44042
VISHAL SINGH <JUST51880@GMAIL.COM>
2026-02-06 14:53:28 +0530

    init existing react project

---
+++ ragbasrd project/src/assets/components/Hero.jsx
@@ -0,0 +1,12 @@
+
+const Hero =() =>{
+
+
+    return(
+<div className=" bg-black  w-screen h-screen" >
+    <h1 className="text-white">dhusuydsuydusfgsdhvdgsdsvhjjdvsjhd</h1>
+  
+</div>)
+}
+
+export default Hero;
